4233a46d0e4cc72253b3c293ee86a1d19b28b917
lutfiah1605 <tugaslutfi.d@gmail.com>
2024-08-28 00:48:12 +0700

    design

--- js/script.js
+++ js/script.js
@@ -1,1 +1,32 @@
 // ini javascript
+function replaceName() {
+    let name = prompt("What's Your Name?", "");
+    document.getElementById("name").innerHTML = name
+}
+
+document.getElementById('tombol').addEventListener("click", function() {
+    replaceName();
+})
+
+function validateform() {
+    const name = document.forms["message-form"]["full-name"].value;
+    const birthDate = document.forms["message-form"]["birth-date"].value;
+    const gender = document.forms["message-form"]["gender"].value;
+    const messages = document.forms["message-form"]["messages"].value;
+
+    if (name == "" || birthDate == "" || gender == "" || messages == "") {
+        alert("Tidak boleh ada yang kosong");
+        return false;
+    }
+
+    setSenderUI(name, birthDate, gender, messages);
+
+    return false;
+}
+
+function setSenderUI(name, birthDate, gender, messages) {
+    document.getElementById("sender-full-name").innerHTML = name;
+    document.getElementById("sender-birth-date").innerHTML = birthDate;
+    document.getElementById("sender-gender").innerHTML = gender;
+    document.getElementById("sender-messages").innerHTML = messages;
+}
